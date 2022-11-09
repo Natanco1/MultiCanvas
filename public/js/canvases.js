557c0f5d6a8ca04ee3255790da24b3ff9df9ff3e
Natanco <nata.oliveira2004@gmail.com>
2022-11-09 15:54:12 -0300

    canvas fabric js events

--- public/js/canvases.js
+++ public/js/canvases.js
@@ -1,9 +1,45 @@
-var canvas = new fabric.Canvas('mog');
-var rect = new fabric.Rect({
-    left: 100,
+const canvas = new fabric.Canvas('mog', {
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight
+});
+
+const red = new fabric.Rect({
+    id: 'cyan',
+    left: 10,
+    top: 10,
+    width: 100,
+    height: 100,
+    fill: 'cyan'
+});
+
+const green = new fabric.Rect({
+    id: 'blue',
+    left: 150,
     top: 150,
-    fill: 'red',
-    width: 200,
-    height: 200
-})
-canvas.add(rect);
+    width: 100,
+    height: 100,
+    fill: 'blue'
+});
+
+
+canvas.on({
+    'object:moving': moved,
+    'object:scaling': resized,
+});
+  
+function moved(obj){
+    console.log(obj.target) 
+}
+
+function resized(obj){
+    console.log(obj)
+}
+
+canvas.add(red);
+canvas.add(green);
+
+
+
+
+canvas.renderAll();
+
